a1040e598c8f35f019a0fff5497165e5655c39a3
AnanduMohan103 <anandhumohan103@gmail.com>
2022-10-13 23:21:16 +0530

    viewfrds

---
+++ src/app/viewfrds/viewfrds.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewfrds',
+  templateUrl: './viewfrds.component.html',
+  styleUrls: ['./viewfrds.component.css']
+})
+export class ViewfrdsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
